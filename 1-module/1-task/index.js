2cf2b0173035f6848ca07603e8a01dd3961b0a55
Lukin Igor <igor.lukin@auxo.team>
2024-08-12 15:16:22 +0300

    Compelete 1st task. Lukin Igor

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  } else if (n > 1) {
+    let result = n;
+    while (n > 1) {
+      result *= --n;
+    }
+    return result;
+  }
 }
