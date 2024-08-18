8949737716035d9db87b7912417bc5885553498b
Игорь Ierneiwel Лукин <liv-work@yandex.ru>
2024-08-18 20:15:49 +0300

    Completed 2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalary = 0;
+  const invalidValues = [null, undefined, NaN, Infinity, -Infinity];
+  for (let salary in salaries) {
+    if (typeof (salaries[salary]) === 'number' &&
+      salaries[salary] > '0' &&
+      !invalidValues.includes(salaries[salary])
+    ) {
+      sumSalary += salaries[salary];
+    }
+  }
+  return sumSalary;
 }
