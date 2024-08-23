9860995a8350a53701358247a2977f0bf1aeb7dc
Igor Ierneiwel Lukin <LIV0609@yandex.ru>
2024-08-23 20:42:05 +0300

    Commit 3-module In Progress

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,18 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
 function namify(users) {
-  // ваш код...
+  const userNames = []
+  for(let user of users){
+    if(user.name){
+      userNames.push(user.name)
+    }
+  }
+  return userNames
 }
+
+let names = namify(users); // ['Вася', 'Петя', 'Маша']
+console.log(names)
