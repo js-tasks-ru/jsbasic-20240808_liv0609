52dbdb7a3cee7d6c7be2a53c5a0fb115ca0cdd20
Igor Ierneiwel Lukin <LIV0609@yandex.ru>
2024-08-23 21:08:34 +0300

    Completed 3-module

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -2,8 +2,8 @@ function showSalary(users, age) {
   let generalSalary = ''
   for (let user of users) {
     if (user.age <= age) {
-      generalSalary += `${user.name}, ${user.balance} \n`
+      generalSalary += `${user.name}, ${user.balance}\n`
     }
   }
-  return generalSalary.slice(0, -2)
+  return generalSalary.slice(0, -1)
 }
