781dd15e0c3a0ec4d8684865e49d5fbd308db8f0
Игорь Ierneiwel Лукин <liv-work@yandex.ru>
2024-09-19 17:15:56 +0300

    completed 6/7 module

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,55 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.modal = this.render();
+    this.isOpen = false;
+    this.handleEsc = this.handleEsc.bind(this);
+  }
+
+  render() {
+    const modal = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__content">
+          <button class="modal__close">&times;</button>
+          <div class="modal__title"></div>
+          <div class="modal__body"></div>
+        </div>
+      </div>
+    `);
+
+    modal.querySelector('.modal__close').addEventListener('click', () => this.close());
+    modal.querySelector('.modal__overlay').addEventListener('click', () => this.close());
+
+    return modal;
+  }
+
+  open() {
+    document.body.appendChild(this.modal);
+    document.body.classList.add('is-modal-open');
+    this.isOpen = true;
+    document.addEventListener('keydown', (event) => this.handleEsc(event));
+  }
+
+  setTitle(title) {
+    this.modal.querySelector('.modal__title').innerText = title;
+  }
+
+  setBody(node) {
+    const modalBody = this.modal.querySelector('.modal__body');
+    modalBody.innerHTML = '';
+    modalBody.appendChild(node);
+  }
+
+  close() {
+    this.modal.remove();
+    document.body.classList.remove('is-modal-open');
+    this.isOpen = false;
+  }
+
+  handleEsc(event) {
+    if (event.code === 'Escape') {
+      this.close();
+    }
   }
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,117 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+
+    this.elem = this.createSlider();
+    this.updateSlider();
+
+    this.addClickHandler();
+    this.addDragHandler();
+  }
+
+  createSlider() {
+    const slider = document.createElement('div');
+    slider.classList.add('slider');
+
+    const thumb = document.createElement('div');
+    thumb.classList.add('slider__thumb');
+
+    const valueDisplay = document.createElement('span');
+    valueDisplay.classList.add('slider__value');
+    thumb.append(valueDisplay);
+
+    const progress = document.createElement('div');
+    progress.classList.add('slider__progress');
+
+    const stepsContainer = document.createElement('div');
+    stepsContainer.classList.add('slider__steps');
+
+    for (let i = 0; i < this.steps; i++) {
+      const step = document.createElement('span');
+      stepsContainer.append(step);
+    }
+
+    slider.append(thumb);
+    slider.append(progress);
+    slider.append(stepsContainer);
+
+    return slider;
+  }
+
+  updateSlider(accurate) {
+    const valueDisplay = this.elem.querySelector('.slider__value');
+    const progress = this.elem.querySelector('.slider__progress');
+    const steps = this.elem.querySelectorAll('.slider__steps span');
+    const thumb = this.elem.querySelector('.slider__thumb');
+
+    valueDisplay.textContent = this.value;
+
+    const percentage = (this.value / (this.steps - 1)) * 100;
+    thumb.style.left = `${percentage - accurate}%`;
+    progress.style.width = `${percentage - accurate}%`;
+
+    steps.forEach((step, index) => {
+      step.classList.toggle('slider__step-active', index === this.value);
+    });
+  }
+
+  addClickHandler() {
+    this.elem.addEventListener('click', (event) => {
+      const rect = this.elem.getBoundingClientRect();
+      const offsetX = event.clientX - rect.left;
+      const totalWidth = rect.width;
+      const stepWidth = totalWidth / (this.steps - 1);
+
+      const newValue = Math.round(offsetX / stepWidth);
+      this.value = Math.max(0, Math.min(newValue, this.steps - 1));
+
+      const accurate = 0;
+      this.updateSlider(accurate);
+
+      const eventChange = new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true,
+      });
+      this.elem.dispatchEvent(eventChange);
+    });
+  }
+
+  addDragHandler() {
+    const thumb = this.elem.querySelector('.slider__thumb');
+
+    thumb.ondragstart = () => false;
+
+    thumb.addEventListener('pointerdown', (event) => {
+      event.preventDefault();
+      this.elem.classList.add('slider_dragging');
+
+      const onPointerMove = (moveEvent) => {
+        const rect = this.elem.getBoundingClientRect();
+        const offsetX = moveEvent.clientX - rect.left;
+        const totalWidth = rect.width;
+        const stepWidth = totalWidth / (this.steps - 1);
+
+        const newValue = Math.round(offsetX / stepWidth);
+        this.value = Math.max(0, Math.min(newValue, this.steps - 1));
+
+        const accurate = 20;
+        this.updateSlider(accurate);
+      };
+
+      const onPointerUp = () => {
+        this.elem.classList.remove('slider_dragging');
+        document.removeEventListener('pointermove', onPointerMove);
+        document.removeEventListener('pointerup', onPointerUp);
+
+        const eventChange = new CustomEvent('slider-change', {
+          detail: this.value,
+          bubbles: true,
+        });
+        this.elem.dispatchEvent(eventChange);
+      };
+      document.addEventListener('pointermove', onPointerMove);
+      document.addEventListener('pointerup', onPointerUp);
+    });
   }
 }
